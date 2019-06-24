0a40e09e78810d20d051466598f54a082d102d47
tsoracco <tomassoracco@gmail.com>
2019-06-23 21:20:19 -0300

    stegano change

--- src/secret_sharing/stegano/include/stegano.h
+++ src/secret_sharing/stegano/include/stegano.h
@@ -4,17 +4,17 @@
 #include <stdint.h>
 #include <unistd.h>
 
-uint8_t recover_lsb(uint8_t *bytes, uint32_t n);
-
 void distribute_lsb(uint8_t byte, uint8_t *pixels, uint32_t n);
 
-void distribute_lsb_width2(uint8_t byte, uint8_t *pixels, size_t pos);
+void lsb_2(uint8_t byte, uint8_t *pixels, size_t pos);
 
-void distribute_lsb_width1(uint8_t byte, uint8_t *pixels, size_t pos);
+void lsb_1(uint8_t byte, uint8_t *pixels, size_t pos);
 
-uint8_t recover_lsb_width2(uint8_t *bytes, size_t pos);
+uint8_t recover_lsb(uint8_t *bytes, uint32_t n);
 
-uint8_t recover_lsb_width1(uint8_t *bytes, size_t pos);
+uint8_t rec_lsb_2(uint8_t *bytes, size_t pos);
+
+uint8_t rec_lsb_1(uint8_t *bytes, size_t pos);
 
 
 #endif
--- src/secret_sharing/stegano/stegano.c
+++ src/secret_sharing/stegano/stegano.c
@@ -11,63 +11,60 @@
 
 void distribute_lsb(uint8_t byte, uint8_t *pixels, uint32_t n){
     if(n == 4) {
-        distribute_lsb_width2(byte, pixels, 0);
+        lsb_2(byte, pixels, 0);
     } else if(n == 8) {
-        distribute_lsb_width1(byte, pixels, 0);
+        lsb_1(byte, pixels, 0);
     }
 }
 
-
-uint8_t recover_lsb(uint8_t *bytes, uint32_t n){
-    if(n == 4) {
-        return recover_lsb_width2(bytes, 0);
-    } else if(n == 8) {
-        return recover_lsb_width1(bytes, 0);
-    }
-
-    printf("recover_lsb has not been implemented for n!=4 && n!=8\n");
-    exit(EXIT_FAILURE);
-}
-
-void distribute_lsb_width2(uint8_t byte, uint8_t *pixels, size_t pos)
-{
+void lsb_2(uint8_t byte, uint8_t *pixels, size_t pos){
     uint32_t j;
-    for (j = 0; j < 8; j += 2)
-    {
+    for (j = 0; j < 8; j += 2){
         uint32_t i = j / 2;
-        uint8_t to_save = (GET_BIT(byte, 8 - 1 - j) << 1) | GET_BIT(byte, 8 - 1 - (j + 1));
-        pixels[i] = (pixels[i] & ~(BIT(pos + 1) | BIT(pos))) | (to_save << pos);
+        uint8_t hide = (GET_BIT(byte, 8 - 1 - j) << 1) | GET_BIT(byte, 8 - 1 - (j + 1));
+        pixels[i] = (pixels[i] & ~(BIT(pos + 1) | BIT(pos))) | (hide << pos);
     }
 }
 
-void distribute_lsb_width1(uint8_t byte, uint8_t *pixels, size_t pos) {
+void lsb_1(uint8_t byte, uint8_t *pixels, size_t pos){
     uint32_t i;
     for (i = 0; i < 8; i++) {
         pixels[i] = (pixels[i] & ~BIT(pos)) | (GET_BIT(byte, 8 - 1 - i) << pos);
     }
 }
 
-uint8_t recover_lsb_width2(uint8_t *bytes, size_t pos)
-{
+uint8_t recover_lsb(uint8_t *bytes, uint32_t n){
+    if(n == 4) {
+        return rec_lsb_2(bytes, 0);
+    } else if(n == 8) {
+        return rec_lsb_1(bytes, 0);
+    }
+
+    printf("Arg error: supported schemes are (2,4) and (4,8)\n");
+    exit(EXIT_FAILURE);
+}
+
+uint8_t rec_lsb_2(uint8_t *bytes, size_t pos){
     uint32_t i;
     uint8_t byte = 0;
-    for (i = 0; i < 4; i++)
-    {
-        uint8_t bits = bytes[i] & (0x03 << pos);
-        bits = bits >> pos;
-        byte |= (bits << (8 - 2 - (i * 2)));
+    uint8_t recover;
+    for (i = 0; i < 4; i++){
+        recover = bytes[i] & (0x03 << pos);
+        recover = recover >> pos;
+        byte |= (recover << (8 - 2 - (i * 2)));
     }
 
     return byte;
 }
 
-uint8_t recover_lsb_width1(uint8_t *bytes, size_t pos) {
+uint8_t rec_lsb_1(uint8_t *bytes, size_t pos){
     uint32_t i;
     uint8_t byte = 0;
+    uint8_t recover;
     for (i = 0; i < 8; i++) {
-        uint8_t bit = bytes[i] & BIT(pos);
-        bit = bit >> pos;
-        byte |= (bit << (8 - 1 - i));
+        recover = bytes[i] & BIT(pos);
+        recover = recover >> pos;
+        byte |= (recover << (8 - 1 - i));
     }
 
     return byte;
